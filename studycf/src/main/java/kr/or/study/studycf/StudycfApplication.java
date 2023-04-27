1aece0a8f30d53776db7ecbdf803bda7b77614f9
dada0417 <jtjt6855@gmail.com>
2023-04-28 00:44:56 +0900

    라이브러리 추가, application.properties 설정

---
+++ studycf/src/main/java/kr/or/study/studycf/StudycfApplication.java
@@ -0,0 +1,13 @@
+package kr.or.study.studycf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudycfApplication.class, args);
+	}
+
+}
---
+++ studycf/src/test/java/kr/or/study/studycf/StudycfApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.study.studycf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
